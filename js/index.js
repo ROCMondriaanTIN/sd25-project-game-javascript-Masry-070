d411dd2e73595ac61150bf003c51342648217fac
Masry-070 <302516027@student.rocmondriaan.nl>
2025-12-05 12:33:22 +0100

    Begin

---
+++ js/index.js
@@ -0,0 +1,44 @@
+let boxes = document.querySelectorAll(".box");
+let coinsA = document.getElementById("coins");
+let start = document.getElementById("startBtn");
+let minesInput = document.getElementById("minesInput");
+let restartBtn = document.getElementById("restartBtn");
+
+let coins = 0;
+
+function beginSpel () {
+    boxes.forEach(box => {
+        box.classList.remove("box");
+        box.classList.add("coverd-box");
+
+        let coverdBX = document.querySelectorAll(".coverd-box");
+        coverdBX.forEach(bx => {
+        bx.addEventListener("click", clickBox)
+})
+
+    })
+        restartBtn.style.display = "block";
+        let minesInputNmbr = Number(minesInput.value);
+        console.log(minesInputNmbr);
+        
+}
+
+function clickBox(e) {
+        e.target.classList.remove("coverd-box");
+        e.target.classList.add("correct-box");
+        coins++
+        coinsA.textContent = `Coins: ${coins}`
+    }
+
+function restart() {
+    boxes.forEach(box => {
+        box.classList.remove("coverd-box");
+        box.classList.remove("correct-box");
+        box.classList.remove("wrong-box");
+        box.classList.add("box");
+        restartBtn.style.display = "none";
+    })
+}
+
+start.addEventListener("click", beginSpel);
+restartBtn.addEventListener("click", restart);
