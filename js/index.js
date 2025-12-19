627fdb4674c59d98055e6dd47b7806b84b47552b
Masry-070 <302516027@student.rocmondriaan.nl>
2025-12-19 17:11:46 +0100

    JS en contact page

--- js/index.js
+++ js/index.js
@@ -6,50 +6,64 @@ let restartBtn = document.getElementById("restartBtn");
 
 let coins = 0;
 let spelActief = false;
+let mineIndex = []
 
 function beginSpel () {
-    let minesInputNmbr = Number(minesInput.value);
-    if (minesInputNmbr >= 7) {
-        alert ("Je kan maximaal 6 Mines hebben.")
+    const minesInputNmbr = Number(minesInput.value);
+    if (minesInputNmbr >= 7, minesInputNmbr <= 0) {
+        alert ("Min 1 Mine -- Max 6 Mines.")
     } else {
         spelActief = true;
         restartBtn.style.display = "block";
         console.log (minesInputNmbr);
-        boxes.forEach(box => {
-        box.classList.remove("box");
-        box.classList.add("coverd-box");
+        mineIndex = []
 
-    let coverdBX = document.querySelectorAll(".coverd-box");
-    coverdBX.forEach(bx => {
-    bx.addEventListener("click", clickBox)
+        boxes.forEach(box => {
+        box.className = "coverd-box"
+        box.addEventListener("click", clickBox);
         })
-        })
+
+        while (mineIndex.length < minesInputNmbr) {
+            const randomMine = Math.floor(Math.random() * boxes.length)
+
+            if (!mineIndex.includes(randomMine)) {
+                mineIndex.push(randomMine)
+            }
+        }
     }
 }
 
 function clickBox(e) {
+    if (!spelActief) return
 
-    if (spelActief === true) {
-        e.target.classList.remove("coverd-box");
-        e.target.classList.add("correct-box");
-        coins++
+    const index = Number(e.target.id)
+
+    if (mineIndex.includes(index)) {
+        e.target.className = "wrong-box"
+        coins --
         coinsA.textContent = `Coins: ${coins}`
-        console.log(e.target.id);
+        spelActief = false
     } else {
-        return
+        e.target.className = "correct-box"
+        coins ++
+        coinsA.textContent = `Coins: ${coins}`
     }
+
     }
 
 function restart() {
     spelActief = false;
+    mineIndex = []
 
     boxes.forEach(box => {
         box.classList.remove("coverd-box");
         box.classList.remove("correct-box");
         box.classList.remove("wrong-box");
         box.classList.add("box");
-        restartBtn.style.display = "none";
+        box.removeEventListener("click", clickBox)
     })
+
+    restartBtn.style.display = "none";
 }
 
 start.addEventListener("click", beginSpel);
